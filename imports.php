6758d913b5162ef4b7a32d22151e34d2265a606f
markmacapinlac <46988808+markmacapinlac@users.noreply.github.com>
2019-02-22 12:04:33 +0800

    Initial commit

---
+++ imports.php
@@ -0,0 +1,15 @@
+
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/node_modules/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/node_modules/animate.css/animate.min.css">
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/node_modules/bulma/css/bulma.min.css">
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/node_modules/easy-autocomplete/dist/easy-autocomplete.min.css">
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/node_modules/bulma-steps/dist/css/bulma-steps.min.css">
+    <script src="<?php echo ROOT_URL; ?>/node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo ROOT_URL; ?>/node_modules/moment/moment.js"></script>
+    <script src="<?php echo ROOT_URL; ?>/node_modules/bulma-steps/dist/js/bulma-steps.min.js"></script>
+    <script src="<?php echo ROOT_URL; ?>/node_modules/easy-autocomplete/dist/jquery.easy-autocomplete.min.js"></script>
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/node_modules/bulma-calendar/dist/css/bulma-calendar.min.css">
+    <script src="<?php echo ROOT_URL; ?>/node_modules/bulma-calendar/dist/js/bulma-calendar.min.js"></script>
+    <script src="<?php echo ROOT_URL; ?>/node_modules/bulma-toast/dist/bulma-toast.min.js"></script>
+    <link rel="stylesheet" href="<?php echo ROOT_URL; ?>/styles.css">
+    <title>Palauig Monitoring Information Management System</title>
---
+++ profile/index.php
@@ -0,0 +1,398 @@
+<?php
+require_once('../access.php');
+require_once(ROOT_DIR.'/connector.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php include(ROOT_DIR."/imports.php"); ?>
+    <script>
+        $(document).ready(function(){
+            var limit = 10;
+            var offset = 0;
+            var count =0;
+            $('[data-trigger="search"]').on('click',function(){
+
+                count = getCount(offset,limit,$('#namesearch').val(),$('#statusselect').val());;
+                var offsetcount = Math.round(count / limit);
+                // console.log(offsetcount)
+                var arrpage = [];
+                var interval = 0;
+                arrpage.push(0);
+                for(var i=0;i<offsetcount-1;i++){
+                    arrpage.push(interval+=limit)
+                }
+                // console.log(arrpage)
+                
+                getData($(this).attr("page-offset"),limit,$('#namesearch').val(),$('#statusselect').val());
+
+                function getCount(xoffset,xlimit,name='',status){
+                    var urlAppend = "";
+                    if(name!=""){
+                        urlAppend +="&name="+name;
+                    }
+                    if(status=='active'){
+                        urlAppend +="&status="+status;
+                    }else if(status=='notactive'){
+                        urlAppend += "&status=notactive";
+                    }else{
+                        urlAppend += "&status=all";
+                    }
+                    var ctr;
+                    $.ajax({
+                        type: "GET",
+                        url: "<?php echo ROOT_URL; ?>/profile/get-count.php?offset="+xoffset+"&limit="+xlimit+urlAppend,
+                        async:false,
+                        success: function (response) {
+                            ctr=response;
+                        }
+                    });
+                    return ctr;
+                }
+                function getData(xoffset,xlimit,name='',status){
+                    var urlAppend = "";
+                    if(name!=""){
+                        urlAppend +="&name="+name;
+                    }
+                    if(status=='active'){
+                        urlAppend +="&status="+status;
+                    }else if(status=='notactive'){
+                        urlAppend += "&status=notactive";
+                    }else{
+                        urlAppend += "&status=all";
+                    }
+
+                    $.ajax({
+                    type: "GET",
+                    url: "<?php echo ROOT_URL; ?>/profile/get-list.php?offset="+xoffset+"&limit="+xlimit+urlAppend,
+                    async:false,
+                    success: function (response) {
+                        var data = JSON.parse(response);
+                        // var data=response;
+                        $('tbody#memberlist *').remove();
+                        $('nav.pagination a').remove();
+                        $('ul.pagination-list li').remove();
+                        console.table(data)
+                        $(data).each(function(index,value){
+                            $('tbody#memberlist').append('\
+                            <tr data-status="'+value.status+'">\
+                                <td class="is-size-7" data-id="'+value.ID+'">\
+                                    <a data-unique="ID" data-infoID="'+value.ID+'"><span class="icon has-text-white">\
+                                        <i class="fa fa-eye"></i>\
+                                    </span></a>\
+                                </td>\
+                                <td class="is-size-7" data-name="'+value.fname+' '+value.mname+' '+value.lname+'">'+value.fname+' '+value.mname+' '+value.lname+'</td>\
+                                <td class="is-size-7" data-age="'+value.dob+'">'+moment(value.dob).format('MMM Do, YYYY')+'</td>\
+                                <td class="is-size-7" data-age="'+value.age+'">'+value.age+'</td>\
+                                <td class="is-size-7" data-sex="'+value.sex+'">'+value.sex+'</td>\
+                                <td class="is-size-7" data-status="'+value.civil_status+'">'+value.civil_status+'</td>\
+                                <td class="is-size-7" data-attainment="'+value.edu_attainment+'">'+value.edu_attainment+'</td>\
+                                <td class="is-size-7" data-occupation="'+value.occupation+'">'+value.occupation+'</td>\
+                                <td class="is-size-7" data-income="'+value.income+'">'+value.income+'</td>\
+                                <td class="is-size-7" data-skills="'+value.skills+'">'+value.skills+'</td>\
+                            </tr>\
+                            ');
+                        })
+
+                        $('nav.pagination').append('\
+                            <a class="pagination-previous" page-offset="0">First</a>\
+                            <a class="pagination-next">Last</a>\
+                        ');
+
+                        $(arrpage).each(function(index,value){
+                            $('.pagination-list').append(
+                                '<li>\
+                                    <a class="pagination-link" page-offset="'+value+'" aria-label="Goto page '+(index+1)+'">'+(index+1)+'</a>\
+                                </li>'
+                            )
+                        })
+                        $('.pagination-next').attr("page-offset",arrpage[arrpage.length-1])
+
+                        $('.pagination a[page-offset="'+offset+'"]').addClass("is-current")
+                        $('.pagination a[page-offset="'+offset+'"]').attr("disabled",true)
+                        $('.pagination a').on('click',function(){                
+                            offset = $(this).attr("page-offset");
+                            getData($(this).attr("page-offset"),limit,$('#namesearch').val(),$('#statusselect').val());
+                        })
+                        
+
+                        $('tbody#memberlist tr').each(function(){
+                            if($(this).attr("data-status")=='ACTIVE'){
+                                $(this).addClass("has-background-primary");
+                                $(this).addClass("has-text-white");
+                            }else
+                            {
+                                $(this).addClass("has-background-danger");
+                                $(this).addClass("has-text-white");
+                            }
+                        })
+                    }
+                });
+                }
+
+                // $('[data-trigger="search"]').on('click',function(){
+                //     count = getCount(offset,limit,$('#namesearch').val(),$('#statusselect').val());
+                //     getData(offset,limit,$('#namesearch').val(),$('#statusselect').val());
+                // })
+                $('a[data-unique="ID"]').on('click',function(){
+                    // console.log($(this).attr("data-infoID"));
+                    $.ajax({
+                        type: "get",
+                        url: "<?php echo ROOT_URL; ?>/profile/get-details.php?ID="+$(this).attr("data-infoID"),
+                        success: function (response) {
+                            var data = JSON.parse(response)[0];
+                            console.clear();
+                            console.table(data)
+                            // $('[modal-toggle="name"]').text("Detailed Information")
+                            $.each( data , function(k,v){
+                                if($('[data-field="'+k+'"]').attr("data-field")!='name'){
+                                    $('[data-field="'+k+'"]').text(v)
+                                }
+                            })
+                            
+                        }
+                    });
+                    $.ajax({
+                        type: "get",
+                        url: "<?php echo ROOT_URL; ?>/profile/get-composition.php?ID="+$(this).attr("data-infoID"),
+                        success: function (response) {
+                            var compo = JSON.parse(response);
+                            console.table(compo);
+                            $('tbody#membercomposition *').remove();
+                            $.each(compo,function(i,v){
+                                console.log(v)
+                                $('tbody#membercomposition').append('\
+                                    <tr>\
+                                        <td>'+v.name+'</td>\
+                                        <td>'+v.relationship+'</td>\
+                                        <td>'+v.c_age+'</td>\
+                                        <td>'+v.c_civil_status+'</td>\
+                                        <td>'+v.c_occupation+'</td>\
+                                        <td>'+v.c_income+'</td>\
+                                    </tr>\
+                                ');
+                            })
+                        }
+                    });
+
+                    $.ajax({
+                        type: "get",
+                        url: "<?php echo ROOT_URL; ?>/profile/get-image.php?ID="+$(this).attr("data-infoID"),
+                        success: function (response) {
+                            var photo = JSON.parse(response)[0];
+                            $('[data-field="image"]').attr('src',photo.imgurl)
+                        }
+                    });
+
+                    
+                    $('[data-modal="viewdetail"]').addClass('is-active');
+                })
+
+            })
+            $('[data-trigger="search"]').trigger('click')
+        })
+    </script>
+</head>
+<body>
+    <?php include(ROOT_DIR."/header.php"); ?>
+    
+    <section class="hero is-medium is-primary is-bold">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                Palauig Monitoring Information Management System
+                </h1>
+                <h2 class="subtitle">
+                    Members List
+                </h2>
+            </div>
+        </div>
+    </section>
+
+    <div id="addform" class="container">   
+    <div class="tile is-ancestor">
+            <div class="tile is-1"></div>
+            <div class="tile is-10 is-vertical">
+
+                <div class="box addnew">
+                        <h1 class="title has-text-centered">Members List</h1>
+                        <div class="columns is-centered">
+                            <div class="column is-6">
+                                <div class="field is-horizontal">
+                                    <div class="field-body">
+                                        <div class="field">
+                                            <label for="">Name:</label>
+                                            <p class="control is-expanded has-icons-left">
+                                                <input id="namesearch" class="input is-small" type="text" placeholder="Name">
+                                                <span class="icon is-small is-left">
+                                                <i class="fa fa-user"></i>
+                                                </span>
+                                            </p>
+                                        </div>
+                                        <div class="field">
+                                            <label for="">Status:</label>
+                                            <p class="control is-expanded has-icons-left">
+                                                <div class="select is-small is-fullwidth">
+                                                    <select id="statusselect">
+                                                        <option value="all">ALL</option>
+                                                        <option value="active">ACTIVE</option>
+                                                        <option value="notactive">NOT ACTIVE</option>
+                                                    </select>
+                                                </div>
+                                            </p>
+                                        </div>
+                                        <div class="field">
+                                            <label for="">&nbsp;</label>
+                                            <p class="control is-expanded has-icons-left">
+                                                <button data-trigger="search" class="button is-small is-link">Search</button>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <table class="table is-hoverable is-fullwidth">
+                            <thead>
+                                <th></th>
+                                <th>FullName</th>
+                                <th>Date of Birth</th>
+                                <th>Age</th>
+                                <th>Gender</th>
+                                <th>Civil Status</th>
+                                <th>Educt'l Attainment</th>
+                                <th>Occupation</th>
+                                <th>Income</th>
+                                <th>Skills</th>
+                            </thead>
+                            <tbody id="memberlist">
+                            </tbody>
+                        </table>
+                        
+                        <div class="paginate">
+                            <nav class="pagination is-rounded" role="navigation" aria-label="pagination">
+                                <a class="pagination-previous" page-offset="0">First</a>
+                                <a class="pagination-next">Last</a>
+                                <ul class="pagination-list">
+                                
+                                    
+                                </ul>
+                            </nav>
+                        </div>
+                </div>
+
+            </div>
+            <div class="tile is-1"></div>
+        </div>
+    </div>
+
+    <div class="modal" data-modal="viewdetail">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+            <header class="modal-card-head">
+            <p class="modal-card-title" modal-toggle="name"></p>
+            <button class="delete" aria-label="close" onclick="$('[data-modal=\'viewdetail\']').removeClass('is-active')"></button>
+            </header>
+            <section class="modal-card-body">
+            <!-- Content ... -->
+                <!-- Image -->
+                <section class="hero is-primary">
+                    <div class="hero-body">
+                        <div class="container">
+                        <h1 class="title">
+                            Detailed Information
+                        </h1>
+                        <h2 class="subtitle">
+                        <br><br>
+                        </h2>
+                        </div>
+                    </div>
+                </section>
+                <div class="level" style="transform: translateY(-70px);">
+                    <div class="level-item"></div>
+                    <div class="level-item">
+                        <figure class="image is-256x256 has-text-centered">
+                            <img data-field="image" style="height: 128px;width: 128px;" class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png">
+                        </figure>
+                    </div>
+                    <div class="level-item"></div>
+                </div>
+                <div class="content" style="transform: translateY(-70px);">
+                    <h2 class="has-text-centered"><strong>Primary Information</strong></h2>
+                    
+                    <table class="table is-fullwidth is-size-7">
+                        <tr>
+                            <td><strong>Name:</strong></td>
+                            <td><span style="display: flex;"><p data-field="fname"></p>&nbsp;<p data-field="mname"></p>&nbsp;<p data-field="lname"></p></span></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Date Of Birth:</strong></td>
+                            <td><p data-field="dob"></p></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Age:</strong></td>
+                            <td><p data-field="age"></p></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Gender:</strong></td>
+                            <td><p data-field="sex"></p></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Civil Status:</strong></td>
+                            <td><p data-field="civil_status"></p></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Educational Attainment:</strong></td>
+                            <td><p data-field="edu_attainment"></p></td>
+                        </tr>
+                        </tr>
+                        <tr>
+                            <td><strong>Occupation:</strong></td>
+                            <td><p data-field="occupation"></p></td>
+                        </tr>
+                        </tr>
+                        <tr>
+                            <td><strong>Income:</strong></td>
+                            <td><p data-field="income"></p></td>
+                        </tr>
+                        </tr>
+                        <tr>
+                            <td><strong>Skills:</strong></td>
+                            <td><p data-field="skills"></p></td>
+                        </tr>
+                    </table>
+
+                    <hr>
+                    <h2 class="has-text-centered"><strong>Relationship Composition</strong></h2>
+
+                    <table class="table is-fullwidth is-size-7">
+                        <thead>
+                            <th>Name</th>
+                            <th>Relationship</th>
+                            <th>Age</th>
+                            <th>Civil Status</th>
+                            <th>Occupation</th>
+                            <th>Income</th>
+                        </thead>
+                        <tbody id="membercomposition">
+                        </tbody>
+                    </table>
+                </div>
+            </section>
+            <footer class="modal-card-foot has-text-right">
+                <button class="button is-dark">
+                    <span class="icon is-small">
+                        <i class="fa fa-print"></i>
+                    </span>
+                    <span>Print</span>
+                </button>
+                <!-- <button class="button">Close</button> -->
+            </footer>
+        </div>
+    </div>
+</body>
+</html>
