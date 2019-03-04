2cf7bf985ea4e0aad92e9a605cf30e16cc5f26a5
markmacapinlac <46988808+markmacapinlac@users.noreply.github.com>
2019-03-04 14:12:05 +0800

    mmis homepage

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
+    <link rel="stylesheet" href="node_modules/bulma/css/bulma.min.css">
+    <link rel="stylesheet" href="styles.css">
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="scripts/login.js"></script>
+    <title>Palauig Monitoring and Information System for Senior Citizen</title>
+</head>
+<body class="has-background-dark" style="height:100vh">
+    <br>
+    <section>
+        <div class="columns">
+            <div class="column is-10 is-offset-1">
+                <h1 class="title has-text-white has-text-centered">Palauig Monitoring and Information System for Senior Citizens</h1>
+            </div>
+        </div>
+    </section>
+    <br><br>
+    <section>
+        <div class="columns">
+            <div class="column is-4 is-offset-4">
+                <div class="box">
+                    <h1 class="title has-text-centered">
+                        Login
+                    </h1>
+                    <form>
+                        <div class="field">
+                            <label for="">Username</label>
+                            <div class="control">
+                                <input type="text" class="input">
+                            </div>
+                        </div>
+                        <div class="field">
+                        <label for="">Password</label>
+                            <div class="control">
+                                <input type="password" class="input">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-link is-pulled-right">Sign-in</button>
+                                <br>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+        
+</body>
+</html>
